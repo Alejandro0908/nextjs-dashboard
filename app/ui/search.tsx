8a2e02d5e1a28ed0360b8064124444d3442108dd
Alejandro0908 <Alejandro1.-@hotmail.com>
2025-05-01 20:04:01 -0600

    Last Commit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,8 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce'; // Importa useDebouncedCallback
-
+import { useDebouncedCallback } from 'use-debounce'; 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -16,12 +15,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
     if (term) {
-      params.set('query', term); // Si hay término, lo establece en la URL
+      params.set('query', term); 
     } else {
-      params.delete('query'); // Si no hay término, lo elimina de la URL
+      params.delete('query'); 
     }
-    replace(`${pathname}?${params.toString()}`); // Actualiza la URL con los nuevos parámetros
-  }, 300); // El debounce es de 300ms
+    replace(`${pathname}?${params.toString()}`); 
+  }, 300); 
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -32,9 +31,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value); // Llama a la función handleSearch con el valor de input
+          handleSearch(e.target.value); 
         }}
-        defaultValue={searchParams.get('query')?.toString()} // Sincroniza el valor con la URL
+        defaultValue={searchParams.get('query')?.toString()} 
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
